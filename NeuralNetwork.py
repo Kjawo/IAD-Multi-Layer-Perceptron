0f69a9da627593edc6e08d28e74cf818683cbdf3
kjawo <ZnX19UvA>
2019-05-09 19:18:54 +0200

    Correct sigmoid

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -2,12 +2,21 @@ import numpy as np
 
 
 def sigmoida(x):
-    return 1 / (1 + np.power(np.e, x))
+    return 1 / (1 + np.exp(-x))
 
 
 class NeuronLayer:
     def __init__(self, ileNeuronow, ileWejscnaNeuron):
         self.wagi = (2 * np.asmatrix(np.random.random((ileNeuronow, ileWejscnaNeuron))) - 1) / 2
+        self.input = np.asmatrix([])
+        self.output = np.asmatrix([])
+
+
+class NeuralNewtwork:
+    def __init__(self, tablicaWarstw):
+        self.layers = tablicaWarstw
+
+    # def feed_forward(self, input):
 
 
 nl = NeuronLayer(4, 2)

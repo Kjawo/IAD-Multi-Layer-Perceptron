06e95e90b5d8fed084d361a5770370cde480163b
Konrad Jaworski <konjaworski@gmail.com>
2019-05-15 17:15:30 +0200

    Divide data into train and test data sets

--- prepData.py
+++ prepData.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn import preprocessing
 
+
 def encoder():
     encoder = np.asmatrix([[1, 0, 0, 0],
                            [0, 1, 0, 0],
@@ -27,14 +28,34 @@ def xor():
 def iris():
     file = pd.read_csv('iris.data', header=None)
     output = file[4]
-    target_matrix = np.zeros((file.shape[0], 3))
 
-    for row in range(output.size):
-        target_matrix[row][output[row]] = 1
+    train_data = file[file.index % 3 != 0]
+    test_data = file[file.index % 3 == 0]
+
+    train_data = train_data.reset_index(drop=True)
+    test_data = test_data.reset_index(drop=True)
+
+    train_target_matrix = np.zeros((train_data.shape[0], 3))
+    test_target_matrix = np.zeros((test_data.shape[0], 3))
+
+    train_output = train_data[4]
+    test_output = test_data[4]
+
+    for row in range(train_output.size):
+        train_target_matrix[row][train_output[row]] = 1
+
+    for row in range(test_output.size):
+        test_target_matrix[row][test_output[row]] = 1
+
+    train_data = train_data.drop(columns=[4])
+    test_data = test_data.drop(columns=[4])
 
     file = file.drop(columns=[4])
 
-    file = pd.DataFrame(preprocessing.scale(file))
+    train_input_matrix = np.asmatrix(train_data.as_matrix())
+    test_input_matrix = np.asmatrix(test_data.as_matrix())
 
-    input_matrix = np.asmatrix(file.as_matrix())
-    return input_matrix, target_matrix
+    train_data = pd.DataFrame(preprocessing.scale(train_data))
+    test_data = pd.DataFrame(preprocessing.scale(test_data))
+
+    return train_input_matrix, train_target_matrix, test_input_matrix, test_target_matrix

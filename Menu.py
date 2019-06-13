3fd483e82cefe0c024bde1bfd4c7a00958fdbf9d
Konrad Jaworski <konjaworski@gmail.com>
2019-06-13 12:16:05 +0200

    Show correct sqrt

--- Menu.py
+++ Menu.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 
 start = time.time()
 
-topology = [16, 1]
-bias = True
+topology = [64, 1]
+bias = False
 _lambda = 0.3
 _momentum = 0.6
 sciezka = 'test'
@@ -20,11 +20,11 @@ sciezka = 'test'
 # nn.learn(100, topology, train_X, train_Y, test_X, test_Y, _lambda, _momentum, bias, 1, 0.001, sciezka, False, True)
 # nn.test(test_X, test_Y, topology, sciezka, False, False)
 
-train_X, train_Y, test_X, test_Y = prepData.sin_data(True)
+train_X, train_Y, test_X, test_Y = prepData.sqrt_data(True)
 plt.plot(test_X, test_Y)
 plt.show()
 
-nn.learn(10, topology, train_X, train_Y, test_X, test_Y, _lambda, _momentum, bias, 1, 0.001, sciezka, False, True)
+nn.learn(50, topology, train_X, train_Y, test_X, test_Y, _lambda, _momentum, bias, 1, 0.001, sciezka, False, True)
 nn.test(test_X, test_Y, topology, sciezka, False, False)
 
 end = time.time()

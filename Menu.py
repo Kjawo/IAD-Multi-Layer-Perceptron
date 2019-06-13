861977f66c060748e817033b0b7fd1ed8a846913
Konrad Jaworski <konjaworski@gmail.com>
2019-06-13 11:55:23 +0200

    Adapt methods calling

--- Menu.py
+++ Menu.py
@@ -3,9 +3,11 @@ import time
 import NeuralNetwork as nn
 import prepData
 
+import matplotlib.pyplot as plt
+
 start = time.time()
 
-topology = [3, 3]
+topology = [16, 1]
 bias = True
 _lambda = 0.3
 _momentum = 0.6
@@ -13,9 +15,16 @@ sciezka = 'test'
 
 # train_input_matrix, train_target_matrix, test_input_matrix, test_target_matrix = prepData.iris()
 
-train_X, train_Y, test_X, test_Y = prepData.seeds()
+# train_X, train_Y, test_X, test_Y = prepData.seeds()
+#
+# nn.learn(100, topology, train_X, train_Y, test_X, test_Y, _lambda, _momentum, bias, 1, 0.001, sciezka, False, True)
+# nn.test(test_X, test_Y, topology, sciezka, False, False)
 
-nn.learn(100, topology, train_X, train_Y, test_X, test_Y, _lambda, _momentum, bias, 1, 0.001, sciezka, False, True)
+train_X, train_Y, test_X, test_Y = prepData.sin_data(True)
+plt.plot(test_X, test_Y)
+plt.show()
+
+nn.learn(10, topology, train_X, train_Y, test_X, test_Y, _lambda, _momentum, bias, 1, 0.001, sciezka, False, True)
 nn.test(test_X, test_Y, topology, sciezka, False, False)
 
 end = time.time()
